0fb81faf2cfc04edd67d2859c4d726a617e6c074
Nick Wesselman <27013789+nickwesselman@users.noreply.github.com>
2024-06-05 13:48:04 -0400

    tweaks to unit test example

--- extensions/snowboard-price-customizer/src/run.test.js
+++ extensions/snowboard-price-customizer/src/run.test.js
@@ -12,39 +12,38 @@ import { run } from './run';
 */
 
 describe('cart transform function', () => {
-  // it('adds cost for everything', () => {
-  //   const result = run({/* TODO: Add test input */});
-
-  //   const expectedPrice = 100 + (5 * 15) + 10 + 15 + 20;
-  //   expect(result.operations[0].update.price.adjustment.fixedPricePerUnit.amount).toEqual(expectedPrice);
-  // });
-
   // it('adds cost for stiffness', () => {
-  //   const result = run({/* TODO: Add test input */});
+  //   const result = run({/* TODO: Add test input with cost.amountPerQuantity.price = 100 */});
     
   //   const expectedPrice = 100 + (3 * 15);
   //   expect(result.operations[0].update.price.adjustment.fixedPricePerUnit.amount).toEqual(expectedPrice);
   // });
 
   // it ('adds cost for size >= 160', () => {
-  //   const result = run({/* TODO: Add test input */});
+  //   const result = run({/* TODO: Add test input with cost.amountPerQuantity.price = 100 */});
 
   //   const expectedPrice = 100 + 10;
   //   expect(result.operations[0].update.price.adjustment.fixedPricePerUnit.amount).toEqual(expectedPrice);
   // });
 
   // it ('adds cost for size ends with W', () => {
-  //   const result = run({/* TODO: Add test input */});
+  //   const result = run({/* TODO: Add test input with cost.amountPerQuantity.price = 100 */});
 
   //   const expectedPrice = 100 + 15;
   //   expect(result.operations[0].update.price.adjustment.fixedPricePerUnit.amount).toEqual(expectedPrice);
   // });
 
   // it('adds cost for sidewall text', () => {
-  //   const result = run({/* TODO: Add test input */});
+  //   const result = run({/* TODO: Add test input with cost.amountPerQuantity.price = 100 */});
 
   //   const expectedPrice = 100 + 20;
   //   expect(result.operations[0].update.price.adjustment.fixedPricePerUnit.amount).toEqual(expectedPrice);
   // });
 
+  // it('adds cost for everything', () => {
+  //   const result = run({/* TODO: Add test input with cost.amountPerQuantity.price = 100 */});
+
+  //   const expectedPrice = 100 + (5 * 15) + 10 + 15 + 20;
+  //   expect(result.operations[0].update.price.adjustment.fixedPricePerUnit.amount).toEqual(expectedPrice);
+  // });
 });
